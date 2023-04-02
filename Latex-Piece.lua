23b5e3b9acd1142e080c4c5fb85a4ece8ca9ce92
mjtbincourse <100660910+mjtbincourse@users.noreply.github.com>
2023-04-02 17:09:29 +0800

    Update Latex-Piece.lua

--- Latex-Piece.lua
+++ Latex-Piece.lua
@@ -27,7 +27,7 @@ Section:NewDropdown(
     " ",
     Weaponlist,
     function(currentOption)
-        Weapon = currentOption
+        _G.Weapon = currentOption
     end
 )
 
@@ -76,7 +76,7 @@ spawn(
                 pcall(
                     function()
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(
-                            game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(Weapon)
+                            game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(_G.Weapon)
                         )
                     end
                 )
@@ -89,20 +89,20 @@ woah:NewToggle(
     "Auto-Attack",
     " ",
     function(q)
-local ToolName = Weapon
-_G.autoattackk = q
-while _G.autoattackk do
-    local tool = game.Players.LocalPlayer.Character:FindFirstChild(ToolName)
-    if tool then
-        tool.Parent = game.Players.LocalPlayer.Backpack
-        tool.Parent = game.Players.LocalPlayer.Character
-        
-        tool:Activate()
-        wait(UseDelay)
-    else
-        wait()
-    end
-end
+        getgenv().ToolName = _G.Weapon
+        _G.autoattackk = q
+        while _G.autoattackk do
+            local tool = game.Players.LocalPlayer.Character:FindFirstChild(ToolName)
+            if tool then
+                tool.Parent = game.Players.LocalPlayer.Backpack
+                tool.Parent = game.Players.LocalPlayer.Character
+
+                tool:Activate()
+                wait(UseDelay)
+            else
+                wait()
+            end
+        end
     end
 )
 
