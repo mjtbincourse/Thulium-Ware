d4b5338a3e308eb94547bf7c85b2604ed6dcf682
mjtbincourse <100660910+mjtbincourse@users.noreply.github.com>
2023-04-05 20:42:41 +0800

    Update Thulium-Loader.lua

--- Thulium-Loader.lua
+++ Thulium-Loader.lua
@@ -1,18 +1,9 @@
-repeat
-    wait()
-until game:IsLoaded()
-
-game:GetService("Players").LocalPlayer.Idled:Connect(
-    function()
-        game:GetService("VirtualUser"):ClickButton2(Vector2.new())
-    end
-)
-
 local thuliumm = "https://raw.githubusercontent.com/mjtbincourse/Thulium-Ware/main/"
 
 local games = {
     [9216815133] = "Trollge%20Conventions.lua",
-    [12860567360] = "Latex-Piece.lua"
+    [12860567360] = "Latex-Piece.lua",
+    [5303541547] = "Two%20Piece.lua"
 }
 
 for i, v in pairs(games) do
@@ -20,3 +11,9 @@ for i, v in pairs(games) do
         loadstring(game:HttpGet(thuliumm .. v))()
     end
 end
+
+game:GetService("Players").LocalPlayer.Idled:Connect(
+    function()
+        game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+    end
+)
